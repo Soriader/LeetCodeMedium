f2c06ce8cc628987ddd2b1c727b8e930553a5798
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-17 22:11:15 +0200

    [~] Task: Letter Combinations of a Phone Number

---
+++ LeetCodeMedium/LetterCombinationsOfAPhoneNumberTask.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeMedium;
+
+public class LetterCombinationsOfAPhoneNumberTask
+{
+    private static Dictionary<char, string> phoneKeyMap = new Dictionary<char, string>()
+    {
+        { '2', "abc" },
+        { '3', "def" },
+        { '4', "ghi" },
+        { '5', "jkl" },
+        { '6', "mno" },
+        { '7', "pqrs" },
+        { '8', "tuv" },
+        { '9', "wxyz" }
+    };
+
+    public static IList<string> LetterCombinations(string digits)
+    {
+        var result = new List<string>();
+        if (string.IsNullOrEmpty(digits))
+        {
+            return result;
+        }
+
+        GenerateCombinations(digits, 0, "", result);
+        return result;
+    }
+
+    private static void GenerateCombinations(string digits, int index, string current, List<string> result)
+    {
+        if (index == digits.Length)
+        {
+            result.Add(current);
+            return;
+        }
+
+        char digit = digits[index];
+        string letters = phoneKeyMap[digit];
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            GenerateCombinations(digits, index + 1, current + letters[i], result);
+        }
+    }
+}
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/1609875896/
---
+++ LeetCodeMediumTests/LetterCombinationsOfAPhoneNumberTest.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeMediumTests;
+
+using LeetCodeMedium;
+
+public class LetterCombinationsOfAPhoneNumberTest
+{
+    [Test]
+    public void EmptyInput()
+    {
+        var result = LetterCombinationsOfAPhoneNumberTask.LetterCombinations("");
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [Test]
+    public void SingleDigit2()
+    {
+        var result = LetterCombinationsOfAPhoneNumberTask.LetterCombinations("2");
+        var expected = new List<string> { "a", "b", "c" };
+        CollectionAssert.AreEquivalent(expected, result);
+    }
+
+    [Test]
+    public void SingleDigit9()
+    {
+        var result = LetterCombinationsOfAPhoneNumberTask.LetterCombinations("9");
+        var expected = new List<string> { "w", "x", "y", "z" };
+        CollectionAssert.AreEquivalent(expected, result);
+    }
+
+    [Test]
+    public void TwoDigits23()
+    {
+        var result = LetterCombinationsOfAPhoneNumberTask.LetterCombinations("23");
+        var expected = new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" };
+        CollectionAssert.AreEquivalent(expected, result);
+    }
+
+    [Test]
+    public void TwoDigits_79()
+    {
+        var result = LetterCombinationsOfAPhoneNumberTask.LetterCombinations("79");
+        var expected = new List<string> 
+        { 
+            "pw", "px", "py", "pz",
+            "qw", "qx", "qy", "qz",
+            "rw", "rx", "ry", "rz",
+            "sw", "sx", "sy", "sz"
+        };
+        CollectionAssert.AreEquivalent(expected, result);
+    }
+
+
+}
