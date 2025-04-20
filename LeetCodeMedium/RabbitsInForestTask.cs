a123e06e9861fb873ecfa9b6df260cae036a3caa
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-20 20:32:47 +0200

    [~] Task: Rabbits in Forest

---
+++ LeetCodeMedium/RabbitsInForestTask.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeMedium;
+
+public class RabbitsInForestTask
+{
+    public static int NumRabbits(int[] answers)
+    {
+        if (answers.Length == 0)
+        {
+            return 0;
+        }
+
+        var answerCount = new Dictionary<int, int>();
+    
+        foreach (var answer in answers)
+        {
+            if (answerCount.ContainsKey(answer))
+            {
+                answerCount[answer]++;
+            }
+            else
+            {
+                answerCount[answer] = 1;
+            }
+        }
+    
+        int totalRabbits = 0;
+
+        foreach (var pair in answerCount)
+        {
+            int x = pair.Key;      
+            int k = pair.Value;    
+        
+            int groups = (k + x) / (x + 1); 
+            totalRabbits += groups * (x + 1);
+        }
+    
+        return totalRabbits;
+    }
+}
+//https://leetcode.com/problems/rabbits-in-forest/description/?envType=daily-question&envId=2025-04-20
---
+++ LeetCodeMediumTests/RabbitsInForestTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeMediumTests;
+
+using LeetCodeMedium;
+
+public class RabbitsInForestTest
+{
+    [Test]
+    public void RabbitsInForest()
+    {
+        Assert.AreEqual(5, RabbitsInForestTask.NumRabbits(new int[] { 1,1,2 }));    
+        Assert.AreEqual(11, RabbitsInForestTask.NumRabbits(new int[] { 10,10,10}));    
+
+    }
+}
