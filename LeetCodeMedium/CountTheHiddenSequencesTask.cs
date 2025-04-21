bbdf845afba9f7516453e0e9ace03814525624bd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-21 19:50:29 +0200

    [~] Task: Count the Hidden Sequences

---
+++ LeetCodeMedium/CountTheHiddenSequencesTask.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeMedium;
+
+public class CountTheHiddenSequencesTask
+{
+    public static int NumberOfArrays(int[] differences, int lower, int upper) 
+    {
+        long minHidden0 = lower;
+        long maxHidden0 = upper;
+        long currentSum = 0;
+    
+        foreach (var diff in differences)
+        {
+            currentSum += diff;
+        
+            minHidden0 = Math.Max(minHidden0, lower - currentSum);
+            maxHidden0 = Math.Min(maxHidden0, upper - currentSum);
+        
+            if (minHidden0 > maxHidden0)
+            {
+                return 0;
+            }        
+        }
+    
+        return (int)(maxHidden0 - minHidden0 + 1);
+    }
+}
+//https://leetcode.com/problems/count-the-hidden-sequences/?envType=daily-question&envId=2025-04-21
---
+++ LeetCodeMediumTests/CountTheHiddenSequencesTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeMediumTests;
+
+using LeetCodeMedium;
+
+public class CountTheHiddenSequencesTest
+{
+    [Test]
+    public void CountTheHiddenSequences()
+    {
+        Assert.AreEqual(2, CountTheHiddenSequencesTask.NumberOfArrays(new int[] { 1,-3,4 }, 1, 6));    
+        Assert.AreEqual(4, CountTheHiddenSequencesTask.NumberOfArrays(new int[] { 3,-4,5,1,-2 }, -4, 5));    
+        Assert.AreEqual(0, CountTheHiddenSequencesTask.NumberOfArrays(new int[] { 4,-7,2 }, 3, 6));    
+    }
+}
