17537108dcfdec91e60f1db536453b21a31c2158
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-22 20:51:53 +0200

    [~] Task: Removing Minimum Number of Magic Beans

---
+++ LeetCodeMedium/RemovingMinimumNumberOfMagicBeansTask.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCodeMedium;
+
+public class RemovingMinimumNumberOfMagicBeansTask
+{
+    public static long MinimumRemoval(int[] beans)
+    {
+        Array.Sort(beans);
+        long total = beans.Sum(b => (long)b);
+        long minRemoval = long.MaxValue;
+    
+        for (int i = 0; i < beans.Length; i++) 
+        {
+            if (i > 0 && beans[i] == beans[i-1])
+            {
+                continue;
+            }
+            
+            long x = beans[i];
+            long count = beans.Length - i;
+            long removed = total - x * count;
+            
+            if (removed < minRemoval) 
+            {
+                minRemoval = removed;
+            }
+        }
+    
+        return minRemoval;
+    }
+    
+}
+
+//https://leetcode.com/problems/removing-minimum-number-of-magic-beans/description/
+
+//Good result but to slow 
+/*public static long MinimumRemoval(int[] beans)
+{
+    int iterator = 0;
+    long result = 0;
+    var boxForBeans = new List<long>();
+
+    while (iterator < beans.Length)
+    {
+        foreach (var bean in beans)
+        {
+            if (bean < beans[iterator])
+            {
+                boxForBeans.Add(bean);
+            }
+            else if (bean > beans[iterator])
+            {
+                    
+                boxForBeans.Add(bean - beans[iterator]);
+
+            }
+        }
+
+        if (boxForBeans.Sum() < result)
+        {
+            result = boxForBeans.Sum();
+        }
+        else if (result == 0)
+        {
+            result = boxForBeans.Sum();
+        }
+
+        iterator++;
+        boxForBeans.Clear();
+    }
+        
+    return result;
+}*/
---
+++ LeetCodeMediumTests/RemovingMinimumNumberOfMagicBeansTest.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeMediumTests;
+
+using LeetCodeMedium;
+
+
+public class RemovingMinimumNumberOfMagicBeansTest
+{
+    [Test]
+    public void RemovingMinimumNumberOfMagicBeans()
+    {
+        Assert.AreEqual(4, RemovingMinimumNumberOfMagicBeansTask.MinimumRemoval(new int[] { 4, 1, 6, 5 }));
+        Assert.AreEqual(7, RemovingMinimumNumberOfMagicBeansTask.MinimumRemoval(new int[] { 2,10,3,2}));
+    }
+}
