3799ac4f556a2d15f18570e1b17b3be22d8f31a0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-30 14:13:19 +0200

    [~] Task: Generate Parentheses

---
+++ LeetCodeMedium/GenerateParenthesesTask.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeMedium;
+
+public class GenerateParenthesesTask
+{
+    public static IList<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        GenerateBracket(n, n, "", result);
+        return result;
+    }
+
+    private static void GenerateBracket(int openRemaining, int closeRemaining, string current, List<string> result)
+    {
+        if (openRemaining == 0 && closeRemaining == 0)
+        {
+            result.Add(current);
+            return;
+        }
+
+        if (openRemaining > 0)
+        {
+            GenerateBracket(openRemaining - 1, closeRemaining, current + "(", result);
+        }
+
+        if (closeRemaining > openRemaining)
+        {
+            GenerateBracket(openRemaining, closeRemaining - 1, current + ")", result);
+        }
+    }
+}
+//https://leetcode.com/problems/generate-parentheses/description/
---
+++ LeetCodeMediumTests/GenerateParenthesesTest.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeMediumTests;
+
+using LeetCodeMedium;
+
+public class GenerateParenthesesTest
+{
+    [Test]
+    public void GenerateParentheses()
+    {
+        Assert.AreEqual(new List<string>() { "((()))", "(()())", "(())()", "()(())", "()()()" },
+            GenerateParenthesesTask.GenerateParenthesis(3));
+
+        Assert.AreEqual(new List<string>() { "()"},
+            GenerateParenthesesTask.GenerateParenthesis(1));
+    }
+}
