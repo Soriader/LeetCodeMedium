8794e2439e04be009af1059ff5a622be35d81667
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-06-29 21:09:11 +0200

    [~] Task: Number of Subsequences That Satisfy the Given Sum Condition

---
+++ LeetCodeMedium/NumberOfSubsequencesThatSatisfyTheGivenSumTask.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeMedium;
+
+public class NumberOfSubsequencesThatSatisfyTheGivenSumTask
+{
+    public static int NumSubseq(int[] nums, int target) 
+    {
+        
+        Array.Sort(nums);
+        int left = 0;
+        int right = nums.Length - 1;
+        int result = 0;
+        int mod = 1000000007;
+    
+        int[] pow = new int[nums.Length + 1];
+        
+        pow[0] = 1;
+        
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            pow[i] = (pow[i - 1] * 2) % mod;
+        }
+    
+        while (left <= right)
+        {
+            if (nums[left] + nums[right] <= target)
+            {
+                result = (result + pow[right - left]) % mod;
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    
+        return result;
+        
+    }
+}
+//https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/?envType=daily-question&envId=2025-06-29
---
+++ LeetCodeMediumTests/NumberOfSubsequencesThatSatisfyTheGivenSumTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeMedium;
+
+namespace LeetCodeMediumTests;
+
+public class NumberOfSubsequencesThatSatisfyTheGivenSumTest
+{
+    [Test]
+    public void NumberOfSubsequencesThatSatisfyTheGivenSum()
+    {
+        Assert.AreEqual(4, NumberOfSubsequencesThatSatisfyTheGivenSumTask.NumSubseq(new int[]{3,5,6,7}, 9));
+    }
+
+}
