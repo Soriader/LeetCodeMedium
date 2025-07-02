0abaf54b165868f876a32d0f826d52c1db87ea69
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-02 19:17:41 +0200

    [~] Task: House Robber

---
+++ LeetCodeMedium/HouseRobberTask.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeMedium;
+
+public class HouseRobberTask
+{
+    public static int Rob(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }        
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }        
+        if (nums.Length == 2)
+        {
+            return nums.Max();
+        }
+        
+        int[] maxMoney = new int[nums.Length];
+        maxMoney[0] = nums[0];
+        maxMoney[1] = Math.Max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.Length; i++)
+        {
+            maxMoney[i] = Math.Max(maxMoney[i - 1], maxMoney[i - 2] + nums[i]);
+        }
+
+        return maxMoney[nums.Length - 1];
+    }
+}
+//https://leetcode.com/problems/house-robber/
---
+++ LeetCodeMediumTests/HouseRobberTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeMedium;
+
+namespace LeetCodeMediumTests;
+
+public class HouseRobberTest
+{
+    [Test]
+    public void HouseRobber()
+    {
+        Assert.AreEqual(4, HouseRobberTask.Rob(new int[]{1,2,3,1}));
+        Assert.AreEqual(12, HouseRobberTask.Rob(new int[]{2,7,9,3,1}));
+    }
+}
