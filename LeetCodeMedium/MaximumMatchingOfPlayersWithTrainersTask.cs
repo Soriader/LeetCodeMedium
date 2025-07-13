a453db0ebdfd01e6c52ed083883bc9e2d25dde16
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-13 14:45:47 +0200

    [~] Task: Maximum Matching of Players With Trainers

---
+++ LeetCodeMedium/MaximumMatchingOfPlayersWithTrainersTask.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeMedium;
+
+public class MaximumMatchingOfPlayersWithTrainersTask
+{
+    public static int MatchPlayersAndTrainers(int[] players, int[] trainers)
+    {
+        Array.Sort(players);
+        Array.Sort(trainers);
+        
+        int result = 0;
+        int playerIndex = 0;
+        int trainerIndex = 0;
+
+        while (playerIndex < players.Length && trainerIndex < trainers.Length)
+        {
+            if (players[playerIndex] <= trainers[trainerIndex])
+            {
+                result++;
+                playerIndex++;
+                trainerIndex++;
+            }
+            else
+            {
+                trainerIndex++;
+            }
+        }
+
+        return result;
+    }
+}
+//https://leetcode.com/problems/maximum-matching-of-players-with-trainers/?envType=daily-question&envId=2025-07-13
---
+++ LeetCodeMediumTests/MaximumMatchingOfPlayersWithTrainersTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeMediumTests;
+
+public class MaximumMatchingOfPlayersWithTrainersTest
+{
+    [Test]
+    public void MaximumMatchingOfPlayersWithTrainers()
+    {
+        Assert.AreEqual(2, MaximumMatchingOfPlayersWithTrainersTask.MatchPlayersAndTrainers(new int[]{4,7,9}, new int[]{ 8, 2, 5, 8 }));
+        Assert.AreEqual(1, MaximumMatchingOfPlayersWithTrainersTask.MatchPlayersAndTrainers(new int[]{1,1,1}, new int[]{10}));
+    }
+}
