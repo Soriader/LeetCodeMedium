24bc41bd3d227917ced56b75707f590ff60dfba7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-16 22:07:57 +0200

    [~] Task: Find the Maximum Length of Valid Subsequence I

---
+++ LeetCodeMedium/FindTheMaximumLengthOfValidSubsequenceITask.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeMedium;
+
+public class FindTheMaximumLengthOfValidSubsequenceITask
+{
+    public static int MaximumLength(int[] nums)
+    {   
+        
+        int[,] dp = new int[2, 2];
+
+        foreach (int x in nums)
+        {
+            int r = x % 2;
+            for (int y = 0; y < 2; y++)
+            {
+                dp[r, y] = dp[y, r] + 1;
+            }
+        }
+
+        int result = 0;
+        
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                result = Math.Max(result, dp[i, j]);
+            }
+        }
+
+        return result;
+    }
+}
+//https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/?envType=daily-question&envId=2025-07-16
---
+++ LeetCodeMediumTests/FindTheMaximumLengthOfValidSubsequenceITest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeMediumTests;
+
+public class FindTheMaximumLengthOfValidSubsequenceITest
+{
+    [Test]
+    public void FindTheMaximumLengthOfValidSubsequenceI()
+    {
+        Assert.AreEqual(4, FindTheMaximumLengthOfValidSubsequenceITask.MaximumLength(new int[]{1,2,3,4}));
+        Assert.AreEqual(6, FindTheMaximumLengthOfValidSubsequenceITask.MaximumLength(new int[]{1,2,1,1,2,1,2}));
+        Assert.AreEqual(2, FindTheMaximumLengthOfValidSubsequenceITask.MaximumLength(new int[]{1,3}));
+        Assert.AreEqual(4, FindTheMaximumLengthOfValidSubsequenceITask.MaximumLength(new int[]{2,13,26,70,76}));
+    }
+}
