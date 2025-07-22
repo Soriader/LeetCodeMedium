17a8387305151384c0d1111035d0d8c4d3130f80
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-07-22 17:52:42 +0200

    [~] Task: Maximum Erasure Value

---
+++ LeetCodeMedium/MaximumErasureValueTask.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeMedium;
+
+public class MaximumErasureValueTask
+{
+    public static int MaximumUniqueSubarray(int[] nums)
+    {
+        var maxSum = 0;
+        var currentSum = 0;
+        var uniqueNumbers = new HashSet<int>();
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            while (uniqueNumbers.Contains(nums[right]))
+            {
+                uniqueNumbers.Remove(nums[left]);
+                currentSum -= nums[left];
+                left++;
+            }
+
+            uniqueNumbers.Add(nums[right]);
+            currentSum += nums[right];
+
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+}
+//https://leetcode.com/problems/maximum-erasure-value/description/?envType=daily-question&envId=2025-07-22
---
+++ LeetCodeMediumTests/MaximumErasureValueTest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeMediumTests;
+
+public class MaximumErasureValueTest
+{
+    [Test]
+    public void MaximumErasureValue()
+    {
+        Assert.AreEqual(17, MaximumErasureValueTask.MaximumUniqueSubarray(new int[]{4,2,4,5,6}));
+        Assert.AreEqual(8, MaximumErasureValueTask.MaximumUniqueSubarray(new int[]{5,2,1,2,5,2,1,2,5}));
+    }
+}
